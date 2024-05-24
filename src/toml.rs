a300a09afad13c606d932b10a8063c6d5a70ef60
sugarflowers <nmtr1p@outlook.jp>
2024-05-24 16:39:47 +0900

    fix

--- src/toml.rs
+++ src/toml.rs
@@ -1,7 +1,7 @@
 use crate::textreader::TextReader;
 use std::collections::HashMap;
 
-pub fn read(file_path &str) -> HashMap<String, HashMap<String, String>> {
+pub fn read(file_path: &str) -> HashMap<String, HashMap<String, String>> {
     let toml_text = TextReader::open(file_path).unwrap().read().unwrap();
     toml::from_str(&toml_text).unwrap()
 }
