4051613589dab31979de97518f103d3ed6da1a14
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2024-11-08 14:41:49 +0900

    Add files via upload
    fix

--- src/toml.rs
+++ src/toml.rs
@@ -1,16 +1,8 @@
 use crate::textreader::TextReader;
 use std::collections::HashMap;
-use anyhow::{Context, Result};
 
 pub fn read(file_path: &str) -> HashMap<String, HashMap<String, String>> {
     let toml_text = TextReader::open(file_path).unwrap().read().unwrap();
-    toml::from_str(&toml_text).unwrap()
-}
-
-pub fn read_check(file_path: &str) -> 
-    Result< HashMap< String, HashMap< String, String > > > {
-
-    let toml_text = TextReader::open(file_path)?.read().unwrap();
     toml::from_str(&toml_text).unwrap()
 }
 
