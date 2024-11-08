06993cce963dd01768758a9c509036d731aa1c1e
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2024-11-08 14:16:20 +0900

    Add files via upload
    add error

--- src/toml.rs
+++ src/toml.rs
@@ -6,3 +6,10 @@ pub fn read(file_path: &str) -> HashMap<String, HashMap<String, String>> {
     toml::from_str(&toml_text).unwrap()
 }
 
+pub fn read_check(file_path: &str) -> 
+    Result<HashMap<String, HashMap<String, String>>, Box<dyn std::error::Error>> {
+
+    let toml_text = TextReader::open(file_path)?.read()?;
+    toml::from_str(&toml_text)?
+}
+
