f0ac045ad1d1f4a7213c6f08f682c6ffeb1c78f8
sugarflowers <128127805+sugarflowers@users.noreply.github.com>
2024-11-11 13:51:10 +0900

    Update toml.rs
    add error check

--- src/toml.rs
+++ src/toml.rs
@@ -1,8 +1,20 @@
 use crate::textreader::TextReader;
 use std::collections::HashMap;
+use anyhow::{Context, Result};
 
 pub fn read(file_path: &str) -> HashMap<String, HashMap<String, String>> {
     let toml_text = TextReader::open(file_path).unwrap().read().unwrap();
     toml::from_str(&toml_text).unwrap()
 }
 
+pub fn read_check(file_path: &str) -> Result<HashMap<String, HashMap<String, String>>> {
+    let toml_text = TextReader::open(file_path)
+        .with_context(|| format!("Failed to open file: {}", file_path))?
+        .read()
+        .with_context(|| format!("Failed to read from file: {}", file_path))?;
+
+    let config: HashMap<String, HashMap<String, String>> = toml::from_str(&toml_text)
+        .context("Failed to parse TOML data")?;
+
+    Ok(config)
+}
